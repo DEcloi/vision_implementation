768b88c66906af98d23962595f5c68679ecdf7ca
swkim <swkim0512@gmail.com>
2019-10-25 16:33:05 +0900

    Optimizer config modify
    Modifying to import modules according to optimizer

--- M2Det/train.py
+++ M2Det/train.py
@@ -35,7 +35,7 @@ def main(config):
 
     # build optimizer, learning rate scheduler. delete every lines containing lr_scheduler for disabling scheduler
     trainable_params = filter(lambda p: p.requires_grad, model.parameters())
-    optimizer = config.init_obj('optimizer', torch.optim, trainable_params)
+    optimizer = config.init_obj('optimizer', config['optimizer']['module'], trainable_params)
 
     lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, optimizer)
 
