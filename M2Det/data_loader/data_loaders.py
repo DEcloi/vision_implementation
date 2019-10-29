99abb19e404c07c4d7970c0ca2f1435c56ddcc1f
swkim <swkim0512@gmail.com>
2019-10-29 17:54:24 +0900

    DataLoader develop
    Develop DataLoader about BDD dataset

--- M2Det/data_loader/data_loaders.py
+++ M2Det/data_loader/data_loaders.py
@@ -1,16 +1,58 @@
+import os
+import cv2
+import json
+
+from PIL import Image
+from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 from base import BaseDataLoader
 
 
-class MnistDataLoader(BaseDataLoader):
+class BDDDataset(Dataset):
+    def __init__(self, image_dir, label_dir, label, transform=None):
+        self.image_dir = image_dir
+        self.transform = transform
+
+        frames = json.load(open(os.path.join(label_dir, label), 'r'))
+
+        self.images = list()
+        self.boxes = list()
+        for frame in frames:
+            box = list()
+            for label in frame['labels']:
+                if 'box2d' not in label:
+                    continue
+                xy = label['box2d']
+                if xy['x1'] >= xy['x2'] or xy['y1'] >= xy['y2']:
+                    continue
+                box.append([xy['x1'], xy['y1'], xy['x2'], xy['y2'], label['category']])
+            self.images.append(frame['name'])
+            self.boxes.append(box)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.image_dir, self.images[idx])
+        image = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(image)
+
+        sample = {'data': image, 'bbox': self.boxes[idx]}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+
+class BDDDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+    def __init__(self, image_dir, label_dir, label, batch_size, shuffle=True, validation_split=0.0, num_workers=1):
         trsfm = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
+            # transforms.Normalize((0.1307,), (0.3081,))
         ])
-        self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        self.dataset = BDDDataset(image_dir, label_dir, label, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
