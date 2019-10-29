13750445a489d394ca10855316d1bfbc18fdd33f
swkim <swkim0512@gmail.com>
2019-10-29 17:56:23 +0900

    Classification and Regression Layer add
    Classification and Regression Layer add

--- M2Det/model/model.py
+++ M2Det/model/model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
@@ -8,7 +9,7 @@ from model import layers, utils
 
 class M2Det(BaseModel):
 
-    def __init__(self, num_classes=10, num_tums=8):
+    def __init__(self, num_classes=10, num_tums=8, phase="train"):
         super(M2Det, self).__init__()
         self.backbone = models.vgg16(pretrained=True).features()
         self.pool5 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
@@ -25,6 +26,20 @@ class M2Det(BaseModel):
 
         self.tum = nn.Sequential(*tum_layers)
         self.sfam = layers.SFAM()
+
+        loc_ = list()
+        conf_ = list()
+        for i in range(self.num_scales):
+            loc_.append(nn.Conv2d(self.planes * self.num_levels,
+                                  4 * 6,  # 4 is coordinates, 6 is anchors for each pixels,
+                                  3, 1, 1))
+            conf_.append(nn.Conv2d(self.planes * self.num_levels,
+                                   self.num_classes * 6,  # 6 is anchors for each pixels,
+                                   3, 1, 1))
+        self.loc = nn.ModuleList(loc_)
+        self.conf = nn.ModuleList(conf_)
+
+        self.phase = phase
 
     def forward(self, x):
         # Backbone network
@@ -49,5 +64,23 @@ class M2Det(BaseModel):
         x = self.sfam(output)
 
         # Classification and Regression
+        loc, conf = list(), list()
+        for (x, l, c) in zip(x, self.loc, self.conf):
+            loc.append(l(x).permute(0, 2, 3, 1).contiguous())
+            conf.append(c(x).permute(0, 2, 3, 1).contiguous())
 
-        return x
+        loc = torch.cat([o.view(o.size(0), -1) for o in loc], 1)
+        conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
+
+        if self.phase == "test":
+            output = (
+                loc.view(loc.size(0), -1, 4),  # loc preds
+                self.softmax(conf.view(-1, self.num_classes)),  # conf preds
+            )
+        else:
+            output = (
+                loc.view(loc.size(0), -1, 4),
+                conf.view(conf.size(0), -1, self.num_classes),
+            )
+
+        return output
